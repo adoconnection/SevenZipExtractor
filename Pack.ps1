2cfb1299d8346c6503989beeaa10ea4080867234
adoconnection <adoconnection@gmail.com>
2016-04-27 16:04:05 +0300

    nuspec

--- Pack.ps1
+++ Pack.ps1
@@ -1,2 +1,5 @@
-h:\nuget pack SevenZipExtractor\SevenZipExtractor.csproj -IncludeReferencedProjects -Prop Configuration=Release
-h:\nuget push SevenZipExtractor.1.0.0.nupkg
+h:
+cd H:\SevenZipExtractorGit\SevenZipExtractor
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild H:\SevenZipExtractorGit\SevenZipExtractor\SevenZipExtractor\SevenZipExtractor.csproj /target:Rebuild /property:Configuration=Release /verbosity:minimal
+h:\nuget pack H:\SevenZipExtractorGit\SevenZipExtractor\SevenZipExtractor\SevenZipExtractor.csproj -IncludeReferencedProjects -Prop Configuration=Release
+h:\nuget push SevenZipExtractor.1.0.0.0.nupkg
