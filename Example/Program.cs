0fccf8edecd2421440bc14f001a54af113b80e66
Mator <why101@msn.com>
2017-04-18 14:06:21 -0700

    adjusted example program a bit

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,18 +8,17 @@ namespace ConsoleApplication86
     {
         static void Main(string[] args)
         {
-            using (ArchiveFile archiveFile = new ArchiveFile(@"archive.7z"))
+            string archiveFileName = @"archive.7z";
+            using (ArchiveFile archiveFile = new ArchiveFile(archiveFileName))
             {
                 foreach (Entry entry in archiveFile.Entries)
                 {
                     if (entry.IsFolder) continue;
                     Console.WriteLine(entry.FileName);
                     Console.WriteLine(string.Format("  Size: {0}", entry.Size));
-                    string directory = Path.GetDirectoryName(entry.FileName);
-                    if (!string.IsNullOrEmpty(directory)) {
-                        Directory.CreateDirectory(directory);
-                    }
-                    entry.Extract(entry.FileName);
+                    string outputPath = Path.Combine("extracted", archiveFileName, entry.FileName);
+                    Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+                    entry.Extract(outputPath);
                 }
             }
 
