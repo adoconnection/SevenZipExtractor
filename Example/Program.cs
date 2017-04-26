de4a4d65dea485cbdc05b4ce60b2523dac3ca9bb
Mator <why101@msn.com>
2017-04-26 09:41:36 -0700

    updated example program to show how to use the Extract method and added comments

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,29 +1,57 @@
 ﻿using System;
 using SevenZipExtractor;
 using System.IO;
+using System.Linq;
 
 namespace ConsoleApplication86
 {
     class Program
     {
+        // you can iterate over the entries in an archive and access their properties
+        static void PrintEntries(ArchiveFile archiveFile)
+        {
+            Console.WriteLine("Entries:");
+            foreach (Entry entry in archiveFile.Entries)
+            {
+                if (entry.IsFolder) continue;
+                Console.WriteLine(entry.FileName);
+                Console.WriteLine(string.Format("  Size: {0}", entry.Size));
+            }
+            Console.WriteLine("");
+        }
+
+        // if you want to extract only a few entries from the archive
+        // use the Entry.Extract method.
+        static void ExtractFirstEntry(ArchiveFile archiveFile)
+        {
+            Entry entry = archiveFile.Entries.First(x => !x.IsFolder);
+            Console.WriteLine("Extracting " + entry.FileName);
+            entry.Extract(Path.GetFileName(entry.FileName));
+            Console.WriteLine("");
+        }
+
+        // if you want to all entries from the archive, use ArchiveFile.Extract
+        static void ExtractAll(string outputPath, ArchiveFile archiveFile) {
+            Console.WriteLine("Extracting all entries to \".\\" + outputPath + "\"");
+            archiveFile.Extract(outputPath, true);
+            Console.WriteLine("");
+        }
+
         static void Main(string[] args)
         {
+            // provide a path to an archive file here
             string archiveFileName = @"archive.7z";
+
+            // do some things with the archive
             using (ArchiveFile archiveFile = new ArchiveFile(archiveFileName))
             {
-                foreach (Entry entry in archiveFile.Entries)
-                {
-                    if (entry.IsFolder) continue;
-                    Console.WriteLine(entry.FileName);
-                    Console.WriteLine(string.Format("  Size: {0}", entry.Size));
-                    string outputPath = Path.Combine("extracted", archiveFileName, entry.FileName);
-                    Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
-                    entry.Extract(outputPath);
-                }
+                PrintEntries(archiveFile);
+                ExtractFirstEntry(archiveFile);
+                ExtractAll(Path.GetFileNameWithoutExtension(archiveFileName), archiveFile);
             }
 
-            Console.WriteLine("");
-            Console.WriteLine("done");
+            // we're done
+            Console.WriteLine("Done");
             Console.ReadKey();
         }
     }
