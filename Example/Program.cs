2519759206d2ff8cca6c141b4fee514fdd83741b
Mator <why101@msn.com>
2017-04-26 11:58:36 -0700

    updated example program with benchmarks and comments on performance

--- Example/Program.cs
+++ Example/Program.cs
@@ -2,6 +2,7 @@
 using SevenZipExtractor;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
 
 namespace ConsoleApplication86
 {
@@ -14,26 +15,42 @@ namespace ConsoleApplication86
             foreach (Entry entry in archiveFile.Entries)
             {
                 if (entry.IsFolder) continue;
-                Console.WriteLine(entry.FileName);
+                Console.WriteLine(string.Format(" {0}", entry.FileName));
                 Console.WriteLine(string.Format("  Size: {0}", entry.Size));
             }
-            Console.WriteLine("");
         }
 
         // if you want to extract only a few entries from the archive
         // use the Entry.Extract method.
-        static void ExtractFirstEntry(ArchiveFile archiveFile)
-        {
+        static void ExtractFirstEntry(ArchiveFile archiveFile) {
             Entry entry = archiveFile.Entries.First(x => !x.IsFolder);
-            Console.WriteLine("Extracting " + entry.FileName);
-            entry.Extract(Path.GetFileName(entry.FileName));
-            Console.WriteLine("");
+            Console.WriteLine("Extracting first entry, \"" + entry.FileName + "\"");
+            entry.Extract(Path.Combine("extracted", Path.GetFileName(entry.FileName)));
         }
 
-        // if you want to all entries from the archive, use ArchiveFile.Extract
+        // note that extracting the last entry is a lot slower than extracting
+        // the first entry because 7-zip has to decompress the archive stream
+        // to seek to the last entry
+        static void ExtractLastEntry(ArchiveFile archiveFile)
+        {
+            Entry entry = archiveFile.Entries.Last(x => !x.IsFolder);
+            Console.WriteLine("Extracting last entry, \"" + entry.FileName + "\"");
+            entry.Extract(Path.Combine("extracted", Path.GetFileName(entry.FileName)));
+        }
+
+        // if you want to extract all entries from the archive, use ArchiveFile.Extract
+        // this is a lot more performant than calling Extract on each entry individually
         static void ExtractAll(string outputPath, ArchiveFile archiveFile) {
             Console.WriteLine("Extracting all entries to \".\\" + outputPath + "\"");
             archiveFile.Extract(outputPath, true);
+        }
+
+        // helper function for benchmarking
+        static void Benchmark(Action procedure) {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            procedure();
+            stopwatch.Stop();
+            Console.WriteLine("Completed in {0} ms", stopwatch.ElapsedMilliseconds);
             Console.WriteLine("");
         }
 
@@ -41,13 +58,16 @@ namespace ConsoleApplication86
         {
             // provide a path to an archive file here
             string archiveFileName = @"archive.7z";
+            string extractTo = Path.Combine("extracted", Path.GetFileNameWithoutExtension(archiveFileName));
+            Directory.CreateDirectory("extracted");
 
             // do some things with the archive
             using (ArchiveFile archiveFile = new ArchiveFile(archiveFileName))
             {
-                PrintEntries(archiveFile);
-                ExtractFirstEntry(archiveFile);
-                ExtractAll(Path.GetFileNameWithoutExtension(archiveFileName), archiveFile);
+                Benchmark(() => PrintEntries(archiveFile));
+                Benchmark(() => ExtractFirstEntry(archiveFile));
+                Benchmark(() => ExtractLastEntry(archiveFile));
+                Benchmark(() => ExtractAll(extractTo, archiveFile));
             }
 
             // we're done
