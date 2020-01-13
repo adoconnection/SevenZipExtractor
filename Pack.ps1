71b0a3b1f0745747418f657af6ae6d2c762a7878
Wojciech Nagórski <wojtpl2@gmail.com>
2020-01-13 22:17:34 +0100

    Update Pack.ps1 script

--- Pack.ps1
+++ Pack.ps1
@@ -1,5 +1,6 @@
-h:
-cd H:\SevenZipExtractorGit\SevenZipExtractor
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild H:\SevenZipExtractorGit\SevenZipExtractor\SevenZipExtractor\SevenZipExtractor.csproj /target:Rebuild /property:Configuration=Release /verbosity:minimal
-h:\nuget pack H:\SevenZipExtractorGit\SevenZipExtractor\SevenZipExtractor\SevenZipExtractor.csproj -IncludeReferencedProjects -Prop Configuration=Release
-h:\nuget push SevenZipExtractor.1.0.15.nupkg -Source https://www.nuget.org/api/v2/package
+dotnet build -c Release
+dotnet test -c Release 
+dotnet pack -c Release -o artifacts SevenZipExtractor\SevenZipExtractor.csproj 
+dotnet nuget push artifacts\SevenZipExtractor.1.0.15.nupkg --source https://www.nuget.org/api/v2/package
+# h:\nuget pack H:\SevenZipExtractorGit\SevenZipExtractor\SevenZipExtractor\SevenZipExtractor.csproj -IncludeReferencedProjects -Prop Configuration=Release
+#h:\nuget push SevenZipExtractor.1.0.15.nupkg -Source https://www.nuget.org/api/v2/package
