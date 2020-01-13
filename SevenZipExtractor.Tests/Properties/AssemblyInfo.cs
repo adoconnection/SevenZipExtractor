a4696d257a4cc5b561e425355ccb0fcd1092dc10
Wojciech Nagórski <wojtpl2@gmail.com>
2020-01-13 22:02:54 +0100

    Remove AssemblyInfo.cs files

--- SevenZipExtractor.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("af401731-d84b-46b3-8542-9e2ffa239d82")]
